ee419b0a0829b47e1d618bf51403d048a54ad244
Williams Amaya <whistemalo@gmail.com>
2022-08-16 00:31:35 -0600

    typo error

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <div className='flex p-10'>
       <h1 className="text-4xl font-bold ">Hello World from google cloud </h1>
-      <h1 className="text-3xl font-bold ">inside a docker cointainer </h1>
+      <h1 className="text-3xl font-bold ">inside a docker container </h1>
       <h1 className="text-2xl font-bold ">that has pulled its code from a github repo </h1>
       <h1 className="text-1xl font-bold ">it also features tailwinds and TYPESCRIPT! </h1>
       </div>
